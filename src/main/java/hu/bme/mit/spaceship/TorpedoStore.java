e7d2077ed1c8d395af9d3e6e701728f125138a29
lauferakos <lauferakos123@gmail.com>
2020-05-10 16:28:30 +0200

    torpedocount javítva

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -41,7 +41,7 @@ public class TorpedoStore {
     double r = this.rand.nextInt();
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
